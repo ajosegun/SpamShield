c7c74fd3139369c333d2a91011ad0fadf991e093
ajosegun <olusegun.ajose@danone.com>
2023-03-20 21:50:17 +0000

    More formatting

--- app.py
+++ app.py
@@ -2,10 +2,7 @@ import streamlit as st
 import core
 
 st.title('SpamShield')
-st.subheader('''SpamShield is an Email/SMS spam classifier built using PySpark's Machine Learning library. 
-                It predicts whether a given message is spam or not. The model is built using Naive Bayes 
-                algorithm and preprocessed using a custom PySpark Pipeline. 
-            ''')
+st.subheader("This an Email/SMS spam classifier built using PySpark's Machine Learning library. It predicts whether a given message is spam or not. The model is built using Naive Bayes algorithm and preprocessed using a custom PySpark Pipeline.")
 
 text = st.text_area("Enter the message")
 if st.button("Predict"):
